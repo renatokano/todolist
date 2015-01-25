4082b98734ee22c32f66deeff73dfcb27060a595
Renato Kano <renato.kenji@hotmail.com>
2015-01-25 05:31:19 -0200

    added security with methods, publish and subscribe resources

--- simple-todos.js
+++ simple-todos.js
@@ -3,6 +3,8 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // This code only runs on the client
+  // At the top of our client code
+  Meteor.subscribe("tasks");
   Template.body.helpers({
     tasks: function () {
       if (Session.get("hideCompleted")){
@@ -19,13 +21,22 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.task.helpers({
+    isOwner: function(){
+      return this.owner === Meteor.userId();
+    }
+  });
+
   Template.body.events({
     "submit .new-task": function(event){
       var text = event.target.text.value; // event.target represents our form element
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
+      // Tasks.insert({
+      //   text: text,
+      //   createdAt: new Date(), // current time
+      //   owner: Meteor.userId(), // _id of logged in user
+      //   username: Meteor.user().username  // username of logged in use  
+      // });
+      Meteor.call("addTask", text);
 
       console.log(event);
 
@@ -42,12 +53,21 @@ if (Meteor.isClient) {
 
   Template.task.events({
     "click .toogle-checked": function(){
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+      //Tasks.update(this._id, {$set: {checked: ! this.checked}});
+      Meteor.call("setChecked", this._id, ! this.checked);
     },
     "click .delete": function(){
-      Tasks.remove(this._id);
+      //Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
+    },
+    "click .toggle-private": function(){
+      Meteor.call("setPrivate",this._id, ! this.private)
     }
   });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  })
 
 }
 
@@ -55,4 +75,51 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  Meteor.publish("tasks", function () {
+    return Tasks.find({
+      $or: [
+        {private: {$ne: true}},
+        {owner: this.userId}
+      ]
+    });
+  });
 }
+
+// At the bottom of simple-todos.js, outside of the client-only block
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+    if(task.owner !== Meteor.userId()){
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if(task.owner !== Meteor.userId()){
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+  setPrivate: function (taskId, setToPrivate){
+    var task = Tasks.findOne(taskId);
+    if (task.owner !== Meteor.userId()){
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId,{$set:{private:setToPrivate}});
+  }
+});
