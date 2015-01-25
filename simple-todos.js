81e3e1e5f47c7c199d18aee90c44c4ef3ebe787d
Renato Kano <renato.kenji@hotmail.com>
2015-01-25 03:03:03 -0200

    added counter for incomplete tasks

--- simple-todos.js
+++ simple-todos.js
@@ -13,6 +13,9 @@ if (Meteor.isClient) {
     },
     hideCompleted: function(){
       return Session.get("hideCompleted");
+    },
+    incompleteCount: function(){
+      return Tasks.find({checked:{$ne:true}}).count();
     }
   });
 
